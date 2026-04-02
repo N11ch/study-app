ca9aa90569bc34316abea733beb18d5c7b94e93e
mdava3776 <mdava3776@gmail.com>
2026-04-02 10:08:39 +0000

    updating the updateProfile function

--- lib/features/auth/screens/update_profile_screen.dart
+++ lib/features/auth/screens/update_profile_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import '../../../core/constants/app_config.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({super.key});
@@ -10,7 +15,60 @@ class UpdateProfileScreen extends StatefulWidget {
 class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   final _formKey = GlobalKey<FormState>();
   final _usernameController = TextEditingController();
+
+  bool _isLoading = false;
   String _selectedRole = 'STUDENT';
+
+  // THIS IS A DANGEROUS ZONE, IF YOU DON'T KNOW WHAT YOUR DOING THEN DON'T TOUCH IT
+  // just pls don't touch this function
+  // todo: implement this method if we already got the new style, atleast we already make it
+  Future<void> _updateProfile() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        // todo: we need to put the body here dog
+        final const RESPONSE = await post(
+          Uri.parse('${AppConfig.API_URL}/user/update/profile'),
+          headers: {'Content-Type': 'application/json'},
+          // body: jsonEncode({
+          //   ''
+          // })
+        );
+
+        if(!mounted) return;
+        final responseData = jsonDecode(RESPONSE.body);
+        print(responseData);
+
+        if(RESPONSE.statusCode == 200 || RESPONSE.statusCode == 201) {
+          ScaffoldMessenger.of(context).showSnackBar(
+           SnackBar(
+            content: Text("Nice!, Your profile has been updated"),
+            backgroundColor: Colors.green
+            ), 
+          );
+
+          // todo RE-ROUTE USER WHERE SHOULD IT GO, BASED ON THE USER ROLES
+          Navigator.of(context).pushReplacementNamed("/");
+        }
+
+      } catch (e) {
+        if (!mounted) return;
+        print("Update Profile error (something error with the API): $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Something went wrong. Please try again later.'),
+            backgroundColor: Colors.redAccent,
+          ),
+        );
+      } finally {
+        if (mounted) setState(() => _isLoading = false);
+      }
+    }
+  }
+  //-------------------------------------
 
   @override
   Widget build(BuildContext context) {
